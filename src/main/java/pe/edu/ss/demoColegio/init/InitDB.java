309dba9be9263577b4f0d750ec3681a7e4201ffa
damsd23 <dverdeguerv@gmail.com>
2019-11-21 19:18:46 -0500

    cuarto commit

--- src/main/java/pe/edu/ss/demoColegio/init/InitDB.java
+++ src/main/java/pe/edu/ss/demoColegio/init/InitDB.java
@@ -1,13 +1,16 @@
 package pe.edu.ss.demoColegio.init;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.logging.java.SimpleFormatter;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import pe.edu.ss.demoColegio.model.entity.Alumno;
 import pe.edu.ss.demoColegio.model.entity.Usuario;
 import pe.edu.ss.demoColegio.model.repository.AuthorityRepository;
 import pe.edu.ss.demoColegio.model.repository.UsuarioRepository;
@@ -42,7 +45,7 @@ public class InitDB implements CommandLineRunner{
 		*/
 		Usuario admin = new Usuario();
 		admin.setUsername("admin");
-		admin.setPassword(passwordEncoder.encode("admin"));
+		admin.setPassword(passwordEncoder.encode("123admin123"));
 		admin.setEnable(true);
 		
 		Usuario beto = new Usuario();
@@ -80,5 +83,19 @@ public class InitDB implements CommandLineRunner{
         */
         List<Usuario> usuarios = Arrays.asList(admin, beto, starchild);        
         this.usuarioRepository.saveAll(usuarios);
+        
+        SimpleDateFormat formatter= new SimpleDateFormat("dd-MM-yyyy");
+      
+        Alumno alumno1=new Alumno();
+        alumno1.setCodigo("ES001");
+        alumno1.setNombre("Diego");
+        alumno1.setApePat("Verdeguer");
+        alumno1.setApeMat("Valderrama");
+        alumno1.setDNI("72870063");
+        alumno1.setEdad(12);
+        alumno1.setDireccion("dverdeguerv@gmail.com");
+        alumno1.setFechaNac(formatter.parse("02-05-2019"));
+        alumno1.setFechaIngreso(formatter.parse("02-05-2019"));
+        
 	}
 }
